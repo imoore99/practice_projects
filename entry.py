049b7ecae51e5d5a3e5985bc89ea21d48957c9f8
Ian Moore <snakelyfe9000@gmail.com>
2022-02-18 11:19:54 -0800

    Added entry field

--- entry.py
+++ entry.py
@@ -3,11 +3,17 @@ from tkinter import *
 
 root = Tk()
 
+e = Entry(root, width=50)
+e.pack()
+e.get()
+##e.insert(0, "Enter your name: ")
+
 def myClick():
-    myLabel = Label(root, text="Look! I clicked a button!")
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text=hello)
     myLabel.pack()
 
-myButton = Button(root, text="Click me!", command=myClick)
+myButton = Button(root, text="Enter your name!", command=myClick)
 myButton.pack()
 
 root.mainloop()
