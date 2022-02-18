f517c4ae20ec1b140125eab14f3ed5cef5737665
Ian Moore <snakelyfe9000@gmail.com>
2022-02-17 17:02:21 -0800

    Add base code for entry.py

---
+++ entry.py
@@ -0,0 +1,14 @@
+
+from tkinter import *
+
+root = Tk()
+
+def myClick():
+    myLabel = Label(root, text="Look! I clicked a button!")
+    myLabel.pack()
+
+myButton = Button(root, text="Click me!", command=myClick)
+myButton.pack()
+
+root.mainloop()
+
